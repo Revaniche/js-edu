c6dcd7bab4f44717f109b2c0a2c0cfa7c01a9dca
Revaniche <revaniche@gmail.com>
2019-10-08 21:03:18 +0400

    create code

--- src/index.js
+++ src/index.js
@@ -8,7 +8,21 @@ module.exports = function getTimeForEducation(
     focus = 'family', 
     knowsProgramming = true,
     config = {family: 4}
-    ) {
-      return 0;
+    ) {var c;
+    if(focus == 'family') {c=config.family} else { if(focus=='friends') {c=config.friends} 
+    else {if (focus=='normal_life') {c=config.normal_life} else {if (focus =='profession') {c=config.profession}
+          else {if (focus == 'carrier') {c=config.carrier} else { if(focus=='top_peformance') {c=config.top_peformance};
+
+            };
+          }; 
+        };
+      };
+    };
+     
+          var a;
+      if (knowsProgramming==true) {a= 800/c} else {a=1300/c};
+      return Math.ceil (a);
+          
   };
+
   
